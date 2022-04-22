49908e63120d4a9b25a0ce1b8ba91bc795df2d15
Patrick <plesaint@yahoo.fr>
2022-04-22 09:18:12 +0200

    maj liste

---
+++ exo-8-2.py
@@ -0,0 +1,23 @@
+# faire une boucle while qui demande des notes à l'élève
+# et les mets dans un tableau tant que la réponse n'est pas "fin"
+# parcourrir cette liste et calculer la moyenne
+# afficher la moyenne de l'eleve
+# "la moyenne de l'elève est x"
+# Afficher en plus la note min et max
+
+notes = []
+somme = 0
+fini = False
+
+while fini == False:
+    reponse = input("Entrez une note ou fin : ")
+    if reponse == "fin":
+        fini = True
+    else:
+        notes.append(float(reponse))
+
+for note in notes:
+    somme = somme + note
+
+moyenne = somme / len(notes)
+print(f"Votre moyenne est de {moyenne}")
--- exo-8.py
+++ exo-8.py
@@ -1,3 +1,9 @@
+# mettre les 3 notes de l'eleve dans une liste
+# parcourrir cette liste et calculer la moyenne
+# afficher la moyenne de l'eleve
+# "la moyenne de l'elève est x"
+# Afficher en plus la note min et max
+
 notes = [12, 5, 9, 8, 17]
 somme = 0
 
